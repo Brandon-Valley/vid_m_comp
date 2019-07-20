86ea726de6d64404d05bfc3afcfb7ab0abbb2942
Brandon Valley <bavvh8@mst.edu>
2019-07-20 14:43:27 -0500

    slider no longer broken but diff str still is

--- gui/GUI_tools/widget_groups/Trim_WG.py
+++ gui/GUI_tools/widget_groups/Trim_WG.py
@@ -19,8 +19,9 @@ class Trim_WG():
         which_scale_moving = StringVar()
         
         def prevent_overlap_and_update_lbls(event=None):
+            print('which_scale_moving.get(): ', which_scale_moving.get())#`````````````````````````````````````````````````````
             def _trying_to_overlap():
-                return self.start_scale.get() < self.end_scale.get() + min_diff or self.end_scale.get() > self.start_scale.get() - min_diff
+                return self.end_scale.get() < self.start_scale.get() + min_diff or self.start_scale.get() > self.end_scale.get() - min_diff
                 
                 
             if _trying_to_overlap():
@@ -49,7 +50,12 @@ class Trim_WG():
         
         start_scale_time_str.set(GUI_tools_utils.sec_to_min_str(start_val.get()))
         end_scale_time_str.set(GUI_tools_utils.sec_to_min_str(end_val.get()))
-        diff_time_str.set(GUI_tools_utils.sec_to_min_str(start_val.get() - end_val.get()))
+        print('in trim_wg,GUI_tools_utils.sec_to_min_str(end_val.get() - start_val.get())    ',GUI_tools_utils.sec_to_min_str(end_val.get() - start_val.get()))
+
+        diff_time_str.set(GUI_tools_utils.sec_to_min_str(end_val.get() - start_val.get()))
+        print('diff_time_str.get(): ', diff_time_str.get())#111111111111111````````````````````````````````````````````````
+        print('in trim_wg,(end_val.get() - start_val.get())', ((end_val.get() - start_val.get())), start_val.get() ,end_val.get())#```````````````
+        print('in trim_wg,GUI_tools_utils.sec_to_min_str(end_val.get() - start_val.get())    ',GUI_tools_utils.sec_to_min_str(end_val.get() - start_val.get()))
         
         
         self.start_lbl = Label(master, textvariable=start_scale_time_str)   # labels that will update
@@ -60,11 +66,10 @@ class Trim_WG():
         def start_scale_clk(event):
             which_scale_moving.set('start_scale')
             
-     
         def end_scale_clk(event):
             which_scale_moving.set('end_scale')
         
-        
+        # bind to mouse clk
         self.start_scale.bind("<Button-1>",start_scale_clk)
         self.end_scale.bind("<Button-1>",end_scale_clk)
     
