68de0c152958b3766bb8db02122e9f0e117bb001
Brandon Valley <bavvh8@mst.edu>
2019-07-20 14:46:10 -0500

    diff lbl correct

--- gui/GUI_tools/widget_groups/Trim_WG.py
+++ gui/GUI_tools/widget_groups/Trim_WG.py
@@ -19,7 +19,7 @@ class Trim_WG():
         which_scale_moving = StringVar()
         
         def prevent_overlap_and_update_lbls(event=None):
-            print('which_scale_moving.get(): ', which_scale_moving.get())#`````````````````````````````````````````````````````
+#             print('which_scale_moving.get(): ', which_scale_moving.get())#`````````````````````````````````````````````````````
             def _trying_to_overlap():
                 return self.end_scale.get() < self.start_scale.get() + min_diff or self.start_scale.get() > self.end_scale.get() - min_diff
                 
@@ -32,7 +32,7 @@ class Trim_WG():
                     
             start_scale_time_str.set(GUI_tools_utils.sec_to_min_str(start_val.get()))
             end_scale_time_str.set(GUI_tools_utils.sec_to_min_str(end_val.get()))
-            diff_time_str.set(GUI_tools_utils.sec_to_min_str(start_val.get() - end_val.get()))
+            diff_time_str.set(GUI_tools_utils.sec_to_min_str(end_val.get() - start_val.get()))
     
         
         start_val = IntVar()  # IntVars to hold
